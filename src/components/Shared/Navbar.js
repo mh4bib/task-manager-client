86e0ba678cc039205a24d289f152e21ec685193c
mahiuddinhabib <mh.habib137@gmail.com>
2022-06-30 19:38:08 +0600

    added navbar

--- src/components/Shared/Navbar.js
+++ src/components/Shared/Navbar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
+    const Menu = <>
+    <li><Link to={'/'}>Home</Link></li>
+    <li><Link to={'/to-do'}>To-Do</Link></li>
+    <li><Link to={'/completed-tasks'}>Completed Tasks</Link></li>
+    <li><Link to={'/calendar'}>Calendar</Link></li>
+    </>
+
     return (
-        <div>
-            <h1>simple navbar</h1>
+        <div class="navbar bg-base-100">
+            <div class="navbar-start">
+                <div class="dropdown">
+                    <label tabindex="0" class="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabindex="0" class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52">
+                        {Menu}
+                    </ul>
+                </div>
+                <a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+            </div>
+            <div class="navbar-center hidden lg:flex">
+                <ul class="menu menu-horizontal p-0">
+                    {Menu}
+                </ul>
+            </div>
+            <div class="navbar-end">
+                <a class="btn">Get started</a>
+            </div>
         </div>
     );
 };
